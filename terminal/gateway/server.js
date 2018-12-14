0f04ec94e5a40b9a818c0e091f42788b47a9fb81
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-12-14 19:12:41 +1100

    Add support for OpenShift auth and restructure code to use async/await.

--- terminal/gateway/server.js
+++ terminal/gateway/server.js
@@ -3,9 +3,13 @@ var basic_auth = require('express-basic-auth')
 var session = require('express-session');
 var uuid = require('uuid');
 var http = require('http');
+var https = require('https');
+var axios = require('axios');
 var path = require('path');
 var url = require('url');
 var fs = require('fs');
+
+var Promise = require('promise');
 
 // Setup the root application. Everything will actually be under a
 // mount point corresponding to the specific user. This is added in
@@ -15,6 +19,10 @@ var app = express();
 
 var uri_root_path = process.env.URI_ROOT_PATH || '';
 
+// In OpenShift we are always behind a proxy, so trust the headers sent.
+
+app.set('trust proxy', true);
+
 // For standalone container deployment, provide the ability to enable
 // authentication using HTTP Basic authentication. In this case there
 // will be no user object added to the client session.
@@ -22,7 +30,9 @@ var uri_root_path = process.env.URI_ROOT_PATH || '';
 var auth_username = process.env.AUTH_USERNAME;
 var auth_password = process.env.AUTH_PASSWORD;
 
-if (auth_username) {
+function install_basic_auth() {
+    console.log('Register basic auth handler');
+
     app.use(basic_auth({
         challenge: true,
         realm: 'Terminal',
@@ -40,6 +50,8 @@ if (auth_username) {
 // being allowed to access the instance. This is so that a change by a
 // JupyterHub admin to status of a user is detected early and they are
 // kicked out.
+
+var handshakes = {}
 
 app.use(session({
     name: 'workshop-session-id',
@@ -66,27 +78,25 @@ var jupyterhub_api_url = process.env.JUPYTERHUB_API_URL;
 var jupyterhub_api_token = process.env.JUPYTERHUB_API_TOKEN;
 var jupyterhub_route = process.env.JUPYTERHUB_ROUTE
 
-var hostname = process.env.HOSTNAME;
-
-if (jupyterhub_client_id) {
+function install_jupyterhub_auth() {
     var api_url = url.parse(jupyterhub_api_url);
 
     var credentials = {
-      client: {
-        id: jupyterhub_client_id,
-        secret: jupyterhub_api_token
-      },
-      auth: {
-        tokenHost: jupyterhub_route,
-        authorizePath: api_url.pathname + '/oauth2/authorize',
-        tokenPath: api_url.pathname + '/oauth2/token'
-      },
-      options: {
-          authorizationMethod: 'body',
-      },
-      http: {
-          rejectUnauthorized: false
-      }
+        client: {
+            id: jupyterhub_client_id,
+            secret: jupyterhub_api_token
+        },
+        auth: {
+            tokenHost: jupyterhub_route,
+            authorizePath: api_url.pathname + '/oauth2/authorize',
+            tokenPath: api_url.pathname + '/oauth2/token'
+        },
+        options: {
+            authorizationMethod: 'body',
+        },
+        http: {
+            rejectUnauthorized: false
+        }
     };
 
     var oauth2 = require('simple-oauth2').create(credentials);
@@ -102,25 +112,18 @@ if (jupyterhub_client_id) {
             var code = req.query.code;
             var state = req.query.state;
 
-            // If there is no tracking of handshakes for this session
-            // return a bad request.
-
-            if (req.session.handshakes === undefined) {
-                return res.status(400).json('No session state');
-            }
-
             // If we seem to have no record of the specific handshake
             // state, redirect back to the main page and start over.
 
-            if (req.session.handshakes[state] === undefined) {
+            if (handshakes[state] === undefined) {
                 return res.redirect(uri_root_path + '/');
             }
 
             // This retrieves the next URL to redirect to from the session
             // for this particular oauth handshake.
 
-            var next_url = req.session.handshakes[state];
-            delete req.session.handshakes[state];
+            var next_url = handshakes[state];
+            delete handshakes[state];
 
             var options = {
                 code: code,
@@ -194,19 +197,8 @@ if (jupyterhub_client_id) {
         // keyed by unique code for this oauth handshake. Use the code
         // as the state for oauth requests.
 
-        if (req.session.handshakes === undefined)
-            req.session.handshakes = {};
-
-        if (Object.keys(req.session.handshakes).length > 50) {
-            // If the number of outstanding auth handshakes gets to be
-            // too many, something fishy going on so clear them all and
-            // start over again.
-
-            req.session.handshakes = {}
-        }
-
-        state = uuid.v4();
-        req.session.handshakes[state] = req.query.next;
+        var state = uuid.v4();
+        handshakes[state] = req.query.next;
 
         const authorization_uri = oauth2.authorizationCode.authorizeURL({
             redirect_uri: uri_root_path + '/oauth_callback',
@@ -221,9 +213,6 @@ if (jupyterhub_client_id) {
     // oauth handshake.
 
     app.use(function (req, res, next) {
-        if (req.session.handshakes === undefined)
-            req.session.handshakes = {};
-
         if (!req.session.user) {
             next_url = encodeURIComponent(req.url);
             res.redirect(uri_root_path + '/oauth_handshake?next=' + next_url);
@@ -234,30 +223,292 @@ if (jupyterhub_client_id) {
     })
 }
 
-// Setup handler for default page. If no overrides of any sort are
-// defined then redirect to /terminal.
+// For authentication using OpenShift OAuth, we perform the handshake
+// and then also need to validate that the user is a member of the
+// project the terminal is deployed in and with appropriate role.
 
-var default_route = process.env.DEFAULT_ROUTE || '/terminal';
+var oauth_service_account = process.env.OAUTH_SERVICE_ACCOUNT;
 
-var default_index = path.join(__dirname, 'routes', 'index.js');
-var override_index = '/opt/app-root/gateway/routes/index.js';
+async function get_oauth_metadata(server) {
+    const options = {
+        baseURL: server,
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+        responseType: 'json'
+    };
 
-if (fs.existsSync(override_index)) {
-    console.log('Set index to', override_index); 
-    app.get('^' + uri_root_path + '/?$', require(override_index));
+    const url = '/.well-known/oauth-authorization-server';
+
+    return (await axios.get(url, options)).data;
 }
-else if (fs.existsSync(default_index)) {
-    console.log('Set index to', default_index); 
-    app.get('^' + uri_root_path + '/?$', require(default_index));
+
+function project_name() {
+    const account_path = '/var/run/secrets/kubernetes.io/serviceaccount';
+    const namespace_path = path.join(account_path, 'namespace');
+
+    return fs.readFileSync(namespace_path, 'utf8');
 }
-else {
-    console.log('Set index to', default_route); 
-    app.get('^' + uri_root_path + '/?$', function (req, res) {
-        res.redirect(uri_root_path + default_route);
+
+function service_account_name(name) {
+    const prefix = 'system:serviceaccount';
+    const namespace = project_name();
+
+    return prefix + ':' + namespace + ':' + name;
+}
+
+function service_account_token() {
+    const account_path = '/var/run/secrets/kubernetes.io/serviceaccount';
+    const token_path = path.join(account_path, 'token');
+
+    return fs.readFileSync(token_path, 'utf8');
+}
+
+function setup_openshift_credentials(metadata, client_id, client_secret) {
+    var credentials = {
+        client: {
+            id: client_id,
+            secret: client_secret
+        },
+        auth: {
+            tokenHost: metadata['issuer'],
+            authorizePath: metadata['authorization_endpoint'],
+            tokenPath: metadata['token_endpoint']
+        },
+        options: {
+            authorizationMethod: 'body',
+        },
+        http: {
+            rejectUnauthorized: false
+        }
+    };
+
+    return credentials;
+}
+
+var kubernetes_host = process.env.KUBERNETES_PORT_443_TCP_ADDR;
+var kubernetes_port = process.env.KUBERNETES_PORT_443_TCP_PORT;
+
+async function get_openshift_user_details(access_token) {
+    const options = {
+        baseURL: 'https://' + kubernetes_host + ':' + kubernetes_port,
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+        headers: { 'Authorization': 'Bearer ' + access_token },
+        responseType: 'json'
+    };
+
+    const url = '/apis/user.openshift.io/v1/users/~';
+
+    var details = (await axios.get(url, options)).data;
+    var name = details['metadata']['name'];
+
+    return name;
+}
+
+async function get_openshift_admin_users() {
+    const namespace = project_name();
+    const token = service_account_token();
+
+    const options = {
+        baseURL: 'https://' + kubernetes_host + ':' + kubernetes_port,
+        httpsAgent: new https.Agent({ rejectUnauthorized: false }),
+        headers: { 'Authorization': 'Bearer ' + token },
+        responseType: 'json'
+    };
+
+    const url = '/apis/authorization.openshift.io/v1/namespaces/' +
+        namespace + '/rolebindings';
+
+    var details = (await axios.get(url, options)).data;
+
+    var users = [];
+
+    for (var i=0; i<details['items'].length; i++) {
+        var rolebinding = details['items'][i];
+        if (rolebinding['roleRef']['name'] == 'admin') {
+            for (var j=0; j<rolebinding['subjects'].length; j++) {
+                var subject = rolebinding['subjects'][j];
+                users.push(subject['name']);
+            }
+        }
+    }
+
+    return users;
+}
+
+async function verify_openshift_user(access_token) {
+    var users = await get_openshift_admin_users();
+
+    console.log('OpenShift admin users', users);
+
+    var name = await get_openshift_user_details(access_token);
+
+    console.log('OpenShift user name', name);
+
+    if (users.includes(name))
+        return name;
+
+    console.log('User forbidden access', name);
+}
+
+function register_openshift_callback(oauth2) {
+    // Define the oauth callback URL. This is the means that the access
+    // token is passed back from JupyterHub for the user. From within
+    // this we also check back with JupyterHub that the user has access
+    // to this instance by fetching the user details and ensuring they
+    // are an admin or they are the user for the instance.
+
+    console.log('Register OAuth callback');
+
+    app.get(uri_root_path + '/oauth_callback', async (req, res) => {
+        try {
+            var code = req.query.code;
+            var state = req.query.state;
+
+            // If we seem to have no record of the specific handshake
+            // state, redirect back to the main page and start over.
+
+            if (handshakes[state] === undefined) {
+                return res.redirect(uri_root_path + '/');
+            }
+
+            // This retrieves the next URL to redirect to from the session
+            // for this particular oauth handshake.
+
+            var next_url = handshakes[state];
+            delete handshakes[state];
+
+            // Obtain the user access token using the authorization code.
+
+            var redirect_uri = [req.protocol, '://', req.hostname,
+                uri_root_path, '/oauth_callback'].join('');
+
+            var options = {
+                redirect_uri: redirect_uri,
+                scope: 'user:info',
+                code: code
+            };
+
+            var auth_result = await oauth2.authorizationCode.getToken(options);
+            var token_result = oauth2.accessToken.create(auth_result);
+
+            console.log('auth_result', auth_result);
+            console.log('token_result', token_result);
+
+            // Now we need to verify whether this user is allowed access
+            // to the project. For this we require that the user have the
+            // admin role in the project since full control of the project
+            // would be given to the container. First we need to work out
+            // who the user is.
+
+            req.session.user = await verify_openshift_user(
+                token_result['token']['access_token']);
+
+            if (!req.session.user) {
+                return res.status(403).json('Forbidden');
+            }
+
+            return res.redirect(next_url);
+        } catch(err) {
+            console.error('Error', err.message);
+            return res.status(500).json('Authentication failed');
+        }
     });
+
+    return oauth2;
 }
 
-// Setup routes for handlers.
+function register_oauth_handshake(oauth2) {
+    console.log('Register OAuth handshake');
+
+    app.get(uri_root_path + '/oauth_handshake', (req, res) => {
+        // Stash the next URL after authentication in the user session
+        // keyed by unique code for this oauth handshake. Use the code
+        // as the state for oauth requests.
+
+        var state = uuid.v4();
+        handshakes[state] = req.query.next;
+
+        var redirect_uri = [req.protocol, '://', req.hostname,
+            uri_root_path, '/oauth_callback'].join('');
+
+        const authorization_uri = oauth2.authorizationCode.authorizeURL({
+            redirect_uri: redirect_uri,
+            scope: 'user:info',
+            state: state
+        });
+
+        res.redirect(authorization_uri);
+    });
+
+    app.use(function (req, res, next) {
+        if (!req.session.user) {
+            next_url = encodeURIComponent(req.url);
+            res.redirect(uri_root_path + '/oauth_handshake?next=' + next_url);
+        }
+        else {
+            next();
+        }
+    })
+}
+
+async function install_openshift_auth() {
+    var server = 'https://openshift.default.svc.cluster.local';
+    var client_id = service_account_name(oauth_service_account);
+    var client_secret = service_account_token();
+
+    var metadata = await get_oauth_metadata(server);
+
+    console.log('OAuth server metadata', metadata);
+
+    var credentials = setup_openshift_credentials(metadata, client_id,
+        client_secret);
+
+    console.log('OAuth server credentials', credentials);
+
+    var oauth2 = require('simple-oauth2').create(credentials);
+
+    register_openshift_callback(oauth2);
+    register_oauth_handshake(oauth2);
+}
+
+async function setup_access() {
+    if (jupyterhub_client_id) {
+        console.log('Install JupyterHub auth support');
+        install_jupyterhub_auth();
+    }
+    else if (oauth_service_account) {
+        console.log('Install OpenShift auth support');
+        await install_openshift_auth();
+    }
+    else if (auth_username) {
+        console.log('Install HTTP Basic auth support');
+        install_basic_auth();
+    }
+}
+
+// Setup handler for default page and routes. If no overrides of any
+// sort are defined then redirect to /terminal.
+
+function set_default_page() {
+    var default_route = process.env.DEFAULT_ROUTE || '/terminal';
+
+    var default_index = path.join(__dirname, 'routes', 'index.js');
+    var override_index = '/opt/app-root/gateway/routes/index.js';
+
+    if (fs.existsSync(override_index)) {
+        console.log('Set index to', override_index); 
+        app.get('^' + uri_root_path + '/?$', require(override_index));
+    }
+    else if (fs.existsSync(default_index)) {
+        console.log('Set index to', default_index); 
+        app.get('^' + uri_root_path + '/?$', require(default_index));
+    }
+    else {
+        console.log('Set index to', default_route); 
+        app.get('^' + uri_root_path + '/?$', function (req, res) {
+            res.redirect(uri_root_path + default_route);
+        });
+    }
+}
 
 function install_routes(directory) {
     if (fs.existsSync(directory)) {
@@ -288,9 +539,31 @@ function install_routes(directory) {
     }
 }
 
-install_routes(path.join(__dirname, 'routes'));
-install_routes('/opt/workshop/gateway/routes');
+function setup_routing() {
+    set_default_page();
 
-// Start listening for requests.
+    install_routes(path.join(__dirname, 'routes'));
+    install_routes('/opt/workshop/gateway/routes');
+}
 
-app.listen(10080);
+// Start the listener.
+
+function start_listener() {
+    console.log('Start listener.');
+
+    app.listen(10080);
+}
+
+// Setup everything and start listener.
+
+async function main() {
+    try {
+        await setup_access();
+        setup_routing();
+        start_listener();
+    } catch (err) {
+        console.log('ERROR', err);
+    }
+}
+
+main();
