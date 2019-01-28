dcdf311cf8c989c00f9f79e076b32fb3195788aa
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-01-28 17:36:38 +0100

    Pass app and prefix when setting up routes, and path prefix to proxy object.

--- terminal/gateway/routes/terminal.js
+++ terminal/gateway/routes/terminal.js
@@ -1,22 +1,25 @@
 var express = require('express');
 var proxy = require('http-proxy-middleware');
+var logger = require('../logger');
 
-// Setup proxying to terminal application. If no terminal session is
-// provided, redirect to session 1. This ensures user always get the
-// same session and not a new one each time if refresh the web browser
-// or access same URL from another browser window.
+module.exports = function(app, prefix) {
+    // Setup proxying to terminal application. If no terminal session is
+    // provided, redirect to session 1. This ensures user always get the
+    // same session and not a new one each time if refresh the web browser
+    // or access same URL from another browser window.
 
-var app = express();
+    var router = express.Router();
 
-app.get('^/?$', function (req, res) {
-    res.redirect(req.baseUrl + '/session/1');
-})
+    router.get('^/?$', function (req, res) {
+        res.redirect(req.baseUrl + '/session/1');
+    })
 
-app.use('/static', express.static('/opt/workshop/butterfly/static'));
+    router.use('/static', express.static('/opt/workshop/butterfly/static'));
 
-app.use(proxy({
-    target: 'http://127.0.0.1:10081',
-    ws: true
-}));
+    router.use(proxy(prefix, {
+        target: 'http://127.0.0.1:10081',
+        ws: true
+    }));
 
-module.exports = app
+    return router;
+}
