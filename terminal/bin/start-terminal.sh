c65da7b3dd48e745b6794f3f5b6bd4d506eb1a3e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-04-04 17:39:48 +1100

    Allow environment variables to be specified via template parameter.

--- terminal/bin/start-terminal.sh
+++ terminal/bin/start-terminal.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+if [ -f /opt/app-root/envvars/terminal_envvars.sh ]; then
+    set -a
+    . /opt/app-root/envvars/terminal_envvars.sh
+    set +a
+fi
+
 exec /opt/workshop/bin/start-butterfly.sh
