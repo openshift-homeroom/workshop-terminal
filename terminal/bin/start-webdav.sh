945d42386dd9f2cef44d10ae0631e2917c6945ca
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-05-20 19:46:08 +1000

    Add WebDAV server support for accessing terminal filesystem.

---
+++ terminal/bin/start-webdav.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eo pipefail
+
+set -x
+
+WEBDAV_PREFIX=${WEBDAV_PREFIX:-${JUPYTERHUB_SERVICE_PREFIX%/}}
+export WEBDAV_PREFIX
+
+WEBDAV_PORT=${WEBDAV_PORT:-10083}
+
+ARGS=""
+
+ARGS="$ARGS --log-to-terminal"
+ARGS="$ARGS --port $WEBDAV_PORT"
+ARGS="$ARGS --application-type static"
+ARGS="$ARGS --include /opt/workshop/etc/httpd-webdav.conf"
+
+source /opt/workshop/webdav/bin/activate
+
+exec mod_wsgi-express start-server $ARGS
