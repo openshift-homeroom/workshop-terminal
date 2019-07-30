f394599e55bbead55179719b1bc76aa082f90459
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-07-30 10:57:45 +1000

    Add workshop only mode.

--- terminal/gateway/routes/terminal.js
+++ terminal/gateway/routes/terminal.js
@@ -2,6 +2,8 @@ var express = require('express');
 var proxy = require('http-proxy-middleware');
 var logger = require('../logger');
 
+var enable_terminal = process.env.ENABLE_TERMINAL;
+
 module.exports = function(app, prefix) {
     // Setup proxying to terminal application. If no terminal session is
     // provided, redirect to session 1. This ensures user always get the
@@ -9,6 +11,10 @@ module.exports = function(app, prefix) {
     // or access same URL from another browser window.
 
     var router = express.Router();
+
+    if (enable_terminal != 'true') {
+        return router;
+    }
 
     router.get('^/?$', function (req, res) {
         res.redirect(req.baseUrl + '/session/1');
